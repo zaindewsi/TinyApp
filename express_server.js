beb40f4aad4621474585c9917bbc98be7c172d3b
Zain Dewsi <zain.dewsi@gmail.com>
2021-05-12 18:29:07 +0000

    replaced body-parser wiht built in express body parser

--- express_server.js
+++ express_server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
-const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const {
   generateRandomString,
@@ -9,10 +8,8 @@ const {
   getUser,
 } = require("./helper-functions/helper-functions");
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
