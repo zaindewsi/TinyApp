2450c4645dbcf770442a01e2efb5eee0a19e2b22
Zain Dewsi <zain.dewsi@gmail.com>
2021-05-13 15:27:10 +0000

    install bcrypt

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
 const {
   generateRandomString,
   isUser,
